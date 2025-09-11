c7471ba2f3da28c78772af091a100eaef6f79c6c
Gopikrishnan G <gkgopikrishnan@gmail.com>
2025-09-11 23:42:18 +0530

    Merge issue

--- scripts/views/create_views.sql
+++ scripts/views/create_views.sql
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-CREATE OR REPLACE VIEW active_employees AS
-=======
 CREATE OR REPLACE VIEW active_employees_2 AS
->>>>>>> 179b543e1bc54c5c36e7fc29728e32d66e613670
 SELECT id, name
 FROM employees
 WHERE is_active = TRUE;
